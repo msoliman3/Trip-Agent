843077b81c486caa8b57a0e028eaa8e825d4aa57
msoliman3 <mohamedymsoliman@gmail.com>
2026-05-19 00:57:40 +0400

    refactored flight agent for langgraph

--- src/agents/flight_agent.py
+++ src/agents/flight_agent.py
@@ -1,14 +1,30 @@
 import serpapi
 import os
 import json
+from src.state import TripState
 client = serpapi.Client(api_key=os.getenv("SERPAPIKEY"))
-results = client.search({
-  "engine": "google_flights",
-  "departure_id": "DXB",
-  "arrival_id": "CAI",
-  "currency": "USD",
-  "type": "2",
-  "outbound_date": "2026-05-20"
-})
 
-print(json.dumps(results["best_flights"], indent=2))
+
+
+def flight_node (state: TripState) -> dict: 
+    try: 
+        departure = state.get("origin")
+        arrival =  state.get("destination")
+        currency = state.get("currency")
+        dates = state.get("dates")
+        out_date = dates.get("start")
+        return_date = dates.get("end")
+
+        results = client.search({
+        "engine": "google_flights",
+        "departure_id": departure,
+        "arrival_id": arrival,
+        "currency": "USD",
+        "type": "1",
+        "outbound_date": out_date,
+        "return_date": return_date
+        })
+        return {"flights": results["best_flights"]}
+    except Exception as e:
+        return {"errors": {"flight_agent": str(e)}}
+
